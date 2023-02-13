03c559937847cb48560f60603c97aaf92d545223
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-13 14:05:37 -0700

    create a package, add git ignore and refactor head to use M&C

--- test/head.test.js
+++ test/head.test.js
@@ -1,8 +1,31 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head");
+const assert = require('chai').assert;
+const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([4]), 4);
-assertEqual(head([4.6,5]), 4.6);
+
+describe("#head", () => {
+ 
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it('returns Hello for ["Hello", "Lighthouse", "Labs"]', ()=> {
+    assert.strictEqual(head( ["Hello", "Lighthouse", "Labs"]),"Hello");
+  });
+
+  it('returns undefined for []', ()=>{
+    assert.strictEqual(head([]),undefined);
+  });
+  
+  it('returns 4 for [4]', ()=>{
+    assert.strictEqual(head([4]),4);
+  });
+  
+  it('returns 4.6 for [4.6,3.2]', ()=>{
+    assert.strictEqual(head([4.6,3.2]),4.6);
+  });
+
+});
